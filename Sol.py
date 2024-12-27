2fa097b8f637bbd2d35f900e4ddadc703bb82083
Aaron Conway <aaron@elastacloud.com>
2024-12-27 19:50:21 +0000

    formatted AU distances into kilometres

--- Sol.py
+++ Sol.py
@@ -1,10 +1,12 @@
 from StarType import Star, StarType, Planet, PlanetType, EllipticalOrbit
+import math
 
 class Sol(Star):
     def __init__(self):
         super().__init__("Sol", 1.0, StarType.G)
 
 class Earth(Planet):
+    AU_IN_KM = 149597879
     def __init__(self, orbit: EllipticalOrbit):
         super().__init__("Earth", 1.0, 1.0, PlanetType.TERRESTRIAL, orbit)
 
@@ -14,4 +16,4 @@ if __name__ == "__main__":
     earth = Earth(earth_orbit)  
     print(f"Closest approach to Earth: {earth.get_closest_approach()}")
     print(f"Farthest approach from Earth: {earth.get_farthest_approach()}")
-    print(earth.get_orbital_distance(36.0)) # February 5th
+    print(f"Orbital distance in km on 5th of Feb: {int(round(earth.get_orbital_distance(36.0) * Earth.AU_IN_KM, 0)): ,} km") # February 5th
